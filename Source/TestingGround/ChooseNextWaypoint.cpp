a9f5e702190b653ee277524df4a6f8519241fc9a
NickyKendy Hua <nicky_kendy@hotmail.com>
2022-06-01 14:10:37 +0800

    C++ BT Task 创建

---
+++ Source/TestingGround/ChooseNextWaypoint.cpp
@@ -0,0 +1,10 @@
+// Copyrights NickyKendy
+
+
+#include "ChooseNextWaypoint.h"
+
+EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	UE_LOG(LogTemp, Warning, TEXT("C++ ChooseNextWaypoint Ready!!!"));
+	return EBTNodeResult::Succeeded;
+}
