1e082a31a625415c6f057bb3be34bb8e12b94544
NickyKendy Hua <nicky_kendy@hotmail.com>
2022-06-18 23:46:03 +0800

    从LFS里整理资源

--- Source/TestingGround/TestingGroundGameMode.cpp
+++ Source/TestingGround/TestingGroundGameMode.cpp
@@ -9,7 +9,7 @@ ATestingGroundGameMode::ATestingGroundGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Blueprints/FPCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Blueprints/Player/FPCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
