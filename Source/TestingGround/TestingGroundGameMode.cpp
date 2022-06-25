059b3648b6e9e3b0befd96597dd852c1d6353b40
NickyKendy Hua <nicky_kendy@hotmail.com>
2022-06-25 15:30:25 +0800

    AI BT重构，命名规范化

--- Source/TestingGround/TestingGroundGameMode.cpp
+++ Source/TestingGround/TestingGroundGameMode.cpp
@@ -9,7 +9,7 @@ ATestingGroundGameMode::ATestingGroundGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("Game/Blueprints/Character/BP_Character"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Blueprints/Character/BP_Character"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
